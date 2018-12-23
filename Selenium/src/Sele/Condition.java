233fb0a7075352936ae9bfb1c19452d2307ecc6c
NagasrinuReddy <nagasrinureddy@gmail.com>
2018-12-23 21:44:00 +0530

    First commit

---
+++ Selenium/src/Sele/Condition.java
@@ -0,0 +1,20 @@
+package Sele;
+
+public class Condition {
+	public static void main(String args[]){
+ int x=100;
+	if(x==20){
+	System.out.println("if statement");		
+	}
+	else if(x==30){
+		System.out.println("elseif statment");
+	}
+	else if(x==40){
+		System.out.println("else if2");
+	}
+	else{
+		System.out.println("else statement");
+	}
+	}
+
+}
